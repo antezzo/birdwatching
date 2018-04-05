66d910f33cae08eb7f84ab53f7eec54bb478ae79
Sam Lam <slam@oberlin.edu>
2018-04-05 19:22:55 -0400

    first commit

---
+++ userdata_scraper.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'twitter'
+require 'yaml'
+
+class TwitterApi
+	attr_reader :client
+
+	desc "collect", "Collect and store tweets using Twitter Search TwitterApi"
+	def initialize
+		keys = YAML.load_file('application.yml')
+
+		@client = Twitter::REST::Client.new do |config|
+			config.consumer_key        = keys['CONSUMER_KEY']
+  			config.consumer_secret     = keys['CONSUMER_KEY_SECRET']
+  			config.access_token        = keys['ACCESS_TOKEN']
+  			config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+		end
+	end
+
+	file = "tweets.txt"
+
+	keywordList = ["follow", "lol", "follower"]
+
+	numTweeters = 10
+
+	target = open(file, 'a+')
+	arr = []
+
+	keywordList.each do |keyword|
+		puts "Scraping #{numTweeters} #{keyword} tweets"
+		client.search("#{keyword} -rt", result_type: "recent").take(numTweeters).collect do |tweet|
+			arr.push(tweet.user.screen_name)
+		end
+	
