e703fd7e797c23032b57e21c697b54188606f1ed
Sam Lam <slam@oberlin.edu>
2018-04-08 19:22:37 -0400

    took out class header and initialize function header

--- userdata_scraper.rb
+++ userdata_scraper.rb
@@ -2,19 +2,19 @@ require 'rubygems'
 require 'twitter'
 require 'yaml'
 
-class TwitterApi
-	attr_reader :client
+#class TwitterApi
+	#attr_reader :client
 
-	def initialize
+	#def initialize
 		keys = YAML.load_file('application.yml')
 
-		@client = Twitter::REST::Client.new do |config|
+		client = Twitter::REST::Client.new do |config|
 			config.consumer_key        = keys['CONSUMER_KEY']
   			config.consumer_secret     = keys['CONSUMER_KEY_SECRET']
   			config.access_token        = keys['ACCESS_TOKEN']
   			config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
 		end
-	end
+	#end
 
 	file = "tweets.txt"
 
@@ -31,4 +31,4 @@ class TwitterApi
 			arr.push(tweet.user.screen_name)
 		end
 	end
-end	
+#end	
