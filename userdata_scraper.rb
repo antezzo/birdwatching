b81dadae6df90532d0e0aa27c8bf3b3122b55cc7
Sam Lam <slam@oberlin.edu>
2018-04-12 14:47:31 -0400

    User scraper workinggit add *

--- userdata_scraper.rb
+++ userdata_scraper.rb
@@ -6,19 +6,19 @@ require 'yaml'
 	#attr_reader :client
 
 	#def initialize
-		keys = YAML.load_file('application.yml')
+	keys = YAML.load_file('application.yml')
 
-		client = Twitter::REST::Client.new do |config|
-			config.consumer_key        = keys['CONSUMER_KEY']
-  			config.consumer_secret     = keys['CONSUMER_KEY_SECRET']
-  			config.access_token        = keys['ACCESS_TOKEN']
-  			config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
-		end
+	client = Twitter::REST::Client.new do |config|
+		config.consumer_key        = keys['CONSUMER_KEY']
+  		config.consumer_secret     = keys['CONSUMER_KEY_SECRET']
+  		config.access_token        = keys['ACCESS_TOKEN']
+  		config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+	end
 	#end
 
 	file = "tweets.txt"
 
-	keywordList = ["follow", "lol", "follower"]
+	keywordList = ["retweet", "lol", "hola"]
 
 	numTweeters = 10
 
@@ -31,4 +31,14 @@ require 'yaml'
 			arr.push(tweet.user.screen_name)
 		end
 	end
+
+	unique = arr.uniq
+	unique.each do |uniquetweet|
+		target.write(uniquetweet)
+		target.write("\n")
+	end
+
+	target.close
+
+	puts "All finished. The usernames have been saved in #{file}"
 #end	
